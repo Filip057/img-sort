6ae797921502f1b3edd5cdb105c55108d078199a
Filip Průdek <101952232+Filip057@users.noreply.github.com>
2023-04-24 16:32:55 +0200

    1.0
    first ver.

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+
+def sort_jpg():
+    name = input("Name of your folder: ")
+    directory = input("Path of your folder: ")
+    old_folder = directory+"/"+name
+    raw_folder = directory+"/"+name+"/RAW"
+    #creating new folder
+    if not os.path.exists(raw_folder):
+        os.makedirs(raw_folder)
+    #iterating over files
+    for file in os.listdir(old_folder):
+        if re.search("NEF", file):
+            os.rename(old_folder+"/"+file, raw_folder+"/"+file)
+
+
+if __name__ == "__main__":
+    sort_jpg()
